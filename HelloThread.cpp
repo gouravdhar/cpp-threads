b436fbf17960e74368fc24ab54aa27762bf8e4fd
gouravdhar <dharg57@gmail.com>
2020-09-20 14:08:59 +0100

    added comments

--- HelloThread.cpp
+++ HelloThread.cpp
@@ -2,16 +2,22 @@
 using namespace std;
 
 void func(){
+	//work to be done by thread
 	for(int i=0; i<10; i++){
 		cout<<"Thread "<<i<<endl;
 	}	
 }
 int main(){
+	//creating thread
 	thread t(&func);
-	// t.detach(); 
+
+	// t.detach(); //comment/uncomment to detach thread
+
+	//check if thread is joinable
 	if(t.joinable()){
 		t.join();
 	}
+
 	cout<<"Main Thread\n";
 	return 0;
 }
