9b36bd3263a9db31ea4434e51e2843426ecf0ec5
gouravdhar <dharg57@gmail.com>
2020-09-21 18:58:55 +0100

    added locks

--- locks.cpp
+++ locks.cpp
@@ -1,44 +1,36 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-vector<int> sharedVariable;
 mutex mtx;
 
 void table(int n)
 {
-	// lock_guard<mutex> guard(mtx);
-	
 	vector<int> temp ;
-	for (int i = 1; i <= 20; i++) {
+	for (int i = 1; i <= 10; i++) {
 		temp.push_back(i*n);
 	}
-
 	
 	lock_guard<mutex> guard(mtx);
-	for (auto itr : temp ) {
-		cout << itr << ", ";
+	
+	// mtx.lock() // comment/uncomment to use lock
+	for (int i=0; i<temp.size(); i++) {
+		cout << n << 'X' << (i+1) << " = "<< temp[i] << endl;
 	}
 	cout<<endl;
+	// mtx.unlock() // comment/uncomment to use lock
 }
 
 
 int main()
 {
-	int max = 50;
-	thread t1(table, 1);
-	thread t2(table, 10);
-	thread t3(table, 20);
-	thread t4(table, 40);
+	thread t1(&table, 1);
+	thread t2(&table, 10);
+	thread t3(&table, 20);
+	thread t4(&table, 40);
+
 	t1.join();
 	t2.join();
 	t3.join();
 	t4.join();
-
-	// thread t5(printList);
-	// t5.join();
-
 	return 0;
 }
-
-
-// lock_guard<mutex> guard(mtx);
